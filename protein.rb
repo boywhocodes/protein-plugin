7afe203dbbc0fed57bbe12a4a7964b951fdfeb9e
Alex Milbert <alex.milbert@gmail.com>
2017-09-15 14:11:42 -0700

    first commit

---
+++ protein.rb
@@ -0,0 +1,19 @@
+def protein_finder
+  puts "Enter price:"
+  price = gets.chomp!
+  price = price.to_f
+  p "$#{price}"
+  puts "Enter quantity"
+  quantity = gets.chomp!
+  quantity = quantity.to_f
+  puts "Enter protein amount (per 1 unit/serving)"
+  protein = gets.chomp!
+  protein = protein.to_f
+
+  pro_per_dol = (protein*quantity) / price
+
+  p "Protein per Dollar: #{pro_per_dol.round(2)}g"
+  p "Price per Gram of Protein: $#{(1/pro_per_dol).round(2)}"
+
+
+end
